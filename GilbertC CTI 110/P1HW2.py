b55667173bd50fd22dd7df54d53893a05bb91ad2
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-08 18:06:49 +0000

    commets

--- GilbertC CTI 110/P1HW2.py
+++ GilbertC CTI 110/P1HW2.py
@@ -3,13 +3,15 @@
 # 9/8/2026
 # Calculating someone's vacation expenses
 
+#Remember to keep all the numbers together
 Budget = float(input("What is your budget for this trip: "))
 Locale = input("Please enter your vacation destination: ")
 Gas = float(input("How much will you be spending on gas: "))
 Board = float(input("How much will you be spending on food: "))
 Room = float(input("How much will you be spending on your accommodations: "))
-Total = float(Gas + Board + Room)
-Leftovers = float(Budget - Total)
+Total = float(Gas + Board + Room) #error due to accidentally adding locale
+Leftovers = float(Budget - Total) #now for the output
+#make it look nice, use print() for spaces between lines
 print("----Travel Expenses----")
 print()
 print("Destination: ",Locale)
@@ -19,3 +21,4 @@ print("Accommodations: $",Room)
 print("Food: $",Board)
 print()
 print("At the end of your vacation at",Locale,",you will have $",Leftovers,"left in your budget.")
+#figure out way to get the $ closer to the numbers
