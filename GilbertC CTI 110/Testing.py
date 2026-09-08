9ff50f198e44bc0e734f4948c52a231304fafeca
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-08 18:08:22 +0000

    spaces

--- GilbertC CTI 110/Testing.py
+++ GilbertC CTI 110/Testing.py
@@ -2,4 +2,4 @@ num,num2,fnum = int,int,int
 num=7
 num2=int(input())
 fnum = num - num2  #use spaces inbetween math operands, these operands are +,-,*,/,//,**(exponet),%(remainder)
-print(num,"multiplied by",num2,"equals",fnum)
+print(num,"multiplied by",num2,"equals",fnum) #/n is for two line spaces
