558d9d6a77e13ffe485a414657d0b4ad953e5675
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-08 17:55:34 +0000

    Starting

---
+++ GilbertC CTI 110/P1HW2.py
@@ -0,0 +1,18 @@
+# Christine Gilbert
+# CTI 110
+# 9/8/2026
+# Calculating someone's vacation expenses
+
+Budget = float(input("What is your budget for this trip: "))
+Locale = input("Please enter your vacation destination: ")
+Gas = float(input("How much will you be spending on gas: "))
+Board = float(input("How much will you be spending on food: "))
+Room = float(input("How much will you be spending on your accommodations: "))
+Total = float(Gas + Board + Room)
+Leftovers = float(Budget - Total)
+print("Travel Expenses")
+print()
+print("Gas: $",Gas)
+print("Accommodations: $",Room)
+
+print("At the end of your vacation at",Locale,",you will have",Leftovers,"left in your budget.")
