311ffc850ddc8d4c032ab0dfd508a2af1e3a6222
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-08 17:30:12 +0000

    Tests

---
+++ GilbertC CTI 110/loops.py
@@ -0,0 +1,6 @@
+next = 4
+for i in range (5)
+    x = int(4)
+    print(x)
+    x = (x - 1)
+    next = (next - 1)
---
+++ Stuff made in Github/1/9/Math.py
@@ -0,0 +1,4 @@
+# Try some simple complex math (this is an oxymoron) through getting input
+print("x^5 + y/5")
+x = float(input("Enter the first variable for x: "))
+y = float(input("Enter the second variable for y: "))
