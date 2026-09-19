2b341cc6e854777e264c38bd910e5b0ec2cbfb97
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-19 22:52:37 +0000

    Final changes

--- GilbertC CTI 110/P2HW2_GilbertChristine.py
+++ GilbertC CTI 110/P2HW2_GilbertChristine.py
@@ -10,6 +10,17 @@ M3 = float(input("Grade for module 3: "))
 M4 = float(input("Grade for module 4: "))
 M5 = float(input("Grade for module 5: "))
 M6 = float(input("Grade for module 6: "))
+#Make the list with the input grades
 grades = {M1,M2,M3,M4,M5,M6}
-print(f'{"Lowest Grade:":<20}{min(grades)}')
-print(f'{"Highest Grade:":<20}{max(grades)}')
+#Do the math
+average = float((M1 + M2 + M3 + M4 + M5 + M6)/6)
+sum = float(M1 + M2 + M3 + M4 + M5 + M6)
+#Print the min and max, average and sum and make it look nice
+print("------------Results--------------")
+print()
+print(f'{"Lowest Grade:":<20}{min(grades):.2f}')
+print(f'{"Highest Grade:":<20}{max(grades):.2f}')
+print(f'{"Average Grade:":<20}{average:.2f}')
+print(f'{"Sum of the Grades:":<20}{sum:.2f}')
+print()
+print("---------------------------------")
