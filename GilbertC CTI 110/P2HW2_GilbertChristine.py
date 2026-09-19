e264388a1d62a69955bdba5f89deef406173279f
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-19 22:52:56 +0000

    Final final changes

--- GilbertC CTI 110/P2HW2_GilbertChristine.py
+++ GilbertC CTI 110/P2HW2_GilbertChristine.py
@@ -4,12 +4,12 @@
 
 #make a list of input grades for many modules then find the min, max and average
 #declare variables for all grades
-M1 = float(input("Grade for module 1: "))
-M2 = float(input("Grade for module 2: "))
-M3 = float(input("Grade for module 3: "))
-M4 = float(input("Grade for module 4: "))
-M5 = float(input("Grade for module 5: "))
-M6 = float(input("Grade for module 6: "))
+M1 = float(input("Grade for Module 1: "))
+M2 = float(input("Grade for Module 2: "))
+M3 = float(input("Grade for Module 3: "))
+M4 = float(input("Grade for Module 4: "))
+M5 = float(input("Grade for Module 5: "))
+M6 = float(input("Grade for Module 6: "))
 #Make the list with the input grades
 grades = {M1,M2,M3,M4,M5,M6}
 #Do the math
